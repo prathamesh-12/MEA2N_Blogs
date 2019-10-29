da74d2be76e5639fb91749306d7ab6dea7124044
Prathamesh V. Nimbkar <pratha662537@majesco.com>
2019-10-29 18:27:18 +0530

    Added Mime type validator for image upload

---
+++ src/app/posts/create-posts/create-post/mime-type.validator.ts
@@ -0,0 +1,23 @@
+import { AbstractControl } from '@angular/forms';
+import { Observable, Observer } from 'rxjs';
+
+// This is async validator
+export const mimeType = (control: AbstractControl): Promise<{[key: string]: any}> | Observable<{[key: string]: any}> => {
+    const file = <File>(control.value);
+    const fileReader = new FileReader();
+    let isValid: boolean = false;
+    //create self observable
+    const fileObs = Observable.create((observer: Observer<{[key: string]: any}>) => {
+        fileReader.addEventListener('loadend', () => {
+            const u8Arr = new Uint8Array(<ArrayBuffer>(fileReader.result)).subarray(0, 4);
+            let header = "";
+            for (let iFile = 0; iFile<u8Arr.length; iFile++) {
+                header += u8Arr[iFile].toString(16);
+            }
+            switch(header) {
+                case "" : isValid = true;
+            }
+        });
+        fileReader.readAsArrayBuffer(file);
+    });
+}
